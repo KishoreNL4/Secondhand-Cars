126fd2fb76255cc625a489351d31c2652b40141d
Kishore <kishoreharry2001@gmail.com>
2024-12-30 21:48:53 +0530

    first commit

--- app/src/firebase-config.js
+++ app/src/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAXnKuGn5zXpEoSN_MzmddhVsBwnV41Ws",
-  authDomain: "carsvalley-d80c0.firebaseapp.com",
-  projectId: "carsvalley-d80c0",
-  storageBucket: "carsvalley-d80c0.appspot.com",
-  messagingSenderId: "596290992278",
-  appId: "1:596290992278:web:96150a1a9375d122f3eab7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
